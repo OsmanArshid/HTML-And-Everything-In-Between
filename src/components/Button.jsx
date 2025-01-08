bccdce84f7a1c7d5b878146834074e920b03cc18
OsmanArshid <usman.arshid01@outlook.com>
2025-01-09 01:34:13 +0500

    made the specialoffers part

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-slate-600 text-white rounded-full border-coral-red" >
+    <button 
+        className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+        ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-slate-600 text-white border-coral-red"} rounded-full w-full" `} >
+        
         {label}
 
         { iconURL && <img
--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,6 +1,50 @@
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+import {arrowRight} from "../assets/icons";
+
+
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      
+      <div className="flex-1">
+        <img
+          className="object-contain w-full"
+          src={offer}
+          width={773}
+          height={687}
+        />
+      </div>
+
+      <div className="flex flex-1 flex-col">        
+          
+          <h2 className="font-palanquin font-bold text-4xl capitalize lg:max-w-lg"> 
+            <span className="text-coral-red"> Special 
+            </span> Offer
+          </h2> 
+          
+          <p className="mt-4 lg:max-w-lg info-text"> 
+            Just do it
+          </p>
+
+          <p className="mt-6 lg:max-w-lg info-text">
+            Our dedication to customers is superb
+          </p>
+          
+          <div className="flex flex-wrap gap-4 mt-11">
+            <Button label="Shop Now"
+              iconURL={arrowRight} />
+
+            <Button 
+              label = "Learn More"
+              backgroundColor = "bg-white"
+              borderColor = "border-slate-gray"
+              textColor = "text-slate-500"
+            />
+          </div>
+
+      </div>
+    </section>
   )
 }
 
